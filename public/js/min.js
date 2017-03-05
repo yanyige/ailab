39cd724dbe020ba64fb7c73a10111d7427457cc7
yanyige <232218719@qq.com>
2017-03-05 11:58:13 +0800

    latest version of ailab

---
+++ public/js/min.js
@@ -0,0 +1,8 @@
+function loopy() {
+var sWord =""
+var sPassword=""
+while (sWord != "cs" || sPassword!="CIT") {              //改为你自己的密码！
+sWord = prompt("username:")
+sPassword= prompt("password:")
+}
+}
